70cdd8389287986afb7018bc3271f52abc591b27
Chris McMeeking <chris.mcmeeking@deque.com>
2017-03-03 20:28:50 -0800

    Initial Commit

--- BasicA11yService/src/main/java/com/moba11y/basiceaccessibilityservice/BasicAccessibilityService.java
+++ BasicA11yService/src/main/java/com/moba11y/basiceaccessibilityservice/BasicAccessibilityService.java
@@ -3,19 +3,26 @@ package com.moba11y.basiceaccessibilityservice;
 import android.accessibilityservice.AccessibilityService;
 import android.view.accessibility.AccessibilityEvent;
 
+import com.chriscm.clog.*;
+import com.moba11y.androida11yutils.A11yNodeInfo;
+
 /**
  * Created by chrismcmeeking on 3/3/17.
  */
 
 public class BasicAccessibilityService extends AccessibilityService {
 
+    static {
+        CLog.initialize(BasicAccessibilityService.class.getSimpleName(), BuildConfig.DEBUG);
+    }
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-
+        CLog.d(event.toString());
+        CLog.d(A11yNodeInfo.wrap(getRootInActiveWindow()).toViewHeirarchy());
     }
 
     @Override
     public void onInterrupt() {
-
+        CLog.e("Service Interrupted: Have never actually had this happen.");
     }
 }
