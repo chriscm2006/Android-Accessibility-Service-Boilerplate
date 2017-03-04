cd3596705507d61659e3c8daa0482466df5b1877
Chris McMeeking <chris.mcmeeking@deque.com>
2017-03-03 20:04:13 -0800

    Initial Commit

---
+++ BasicA11yService/src/main/java/com/moba11y/basiceaccessibilityservice/BasicAccessibilityService.java
@@ -0,0 +1,21 @@
+package com.moba11y.basiceaccessibilityservice;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * Created by chrismcmeeking on 3/3/17.
+ */
+
+public class BasicAccessibilityService extends AccessibilityService {
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
